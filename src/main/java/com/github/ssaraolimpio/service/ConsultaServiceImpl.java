32dbe1e480b21e212ad07a67eec64dc2d2ab5e36
ssaraolimpio <saraevelin08@gmail.com>
2025-06-10 22:30:52 -0300

    validando regras de negocio de um agendamento de consulta

--- src/main/java/com/github/ssaraolimpio/service/ConsultaServiceImpl.java
+++ src/main/java/com/github/ssaraolimpio/service/ConsultaServiceImpl.java
@@ -5,7 +5,6 @@ import com.github.ssaraolimpio.model.Consulta;
 import com.github.ssaraolimpio.repository.ConsultaRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 
 
@@ -22,22 +21,19 @@ public abstract class ConsultaServiceImpl implements ConsultaService {
     @Override
     public Consulta agendarConsulta(ConsultaDTO consultaDTO) {
 
-        // convertendo DTO para model
-        Consulta consulta = new Consulta(consultaDTO);
-
         /* TODO: criar métodos que validam regras de negócio
          *   1. agendamento não pode ser data retroativa ✅
-         *   2. a consulta deve estar dentro do horário de atendimento do médico
+         *   2. a consulta deve estar dentro do horário de atendimento do médico ✅
          *   3. médico só pode atender 1 paciente por vez (1:1)
          * */
 
+        // convertendo DTO para model
+        Consulta consulta = new Consulta(consultaDTO);
+
         try {
-            String data = consulta.getData();
-            String hora = consulta.getHora();
-
-            LocalDateTime dataHoraAgendada = LocalDateTime.parse(data + "T" + hora);
-
-            this.validaDataAgendamento(dataHoraAgendada);
+            // valida data e hora do agendamento da consutla
+            LocalDateTime dataHoraConsulta = LocalDateTime.parse(consulta.getData() + "T" + consulta.getHora());
+            this.validaDataAgendamento(dataHoraConsulta);
 
         } catch (Exception e) {
             throw new RuntimeException("Erro ao validar data/hora da consulta",e);
@@ -46,13 +42,14 @@ public abstract class ConsultaServiceImpl implements ConsultaService {
         return repository.save(consulta);
     }
 
-    public void validaDataAgendamento(LocalDateTime dataHoraAgendada) throws Exception {
+    public void validaDataAgendamento(LocalDateTime dataHoraConsulta) throws Exception {
+
         try {
-            if (dataHoraAgendada.isBefore(LocalDateTime.now())) {
+            if (dataHoraConsulta.isBefore(LocalDateTime.now())) {
                 throw new IllegalArgumentException("A data e hora da consulta não pode ser no passado.");
             }
 
-            int hora = dataHoraAgendada.getHour();
+            int hora = dataHoraConsulta.getHour();
             if (hora < 8 || hora >= 18) {
                 throw new IllegalArgumentException("A consulta deve ser entre às 08:00 e 18:00.");
             }
